06b4270aa66ba1c93cfddd6ffcb0ecda4067e93e
BECKLESPINAX <germanosaurio@hotmail.com>
2024-10-12 00:34:25 -0300

    Add files via upload

---
+++ MegalÖSoftMyPastilla/MegalÖSoftMyPastilla.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MegalÖSoftMyPastilla
+{
+	public class MSMyPastilla
+	{
+	}
+}
---
+++ MegalÖSoftMyPastilla/MegalÖSoftMyPastillaDirect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using MegalÖSoftMyPastillaStructure;
+
+namespace MegalÖSoftMyPastilla
+{
+public class MSMyPastillaDirect
+{
+protected SQLiteCommand sqlitecommand;
+protected SQLiteConnection sqliteconnection;
+protected SQLiteDataReader sqlitereader;
+
+protected string squery = "";
+protected string cs = "";//Cadena de conexión
+protected bool breturn=false;
+protected int iresult = 0;
+protected string scommandinsert = "", scommandupdate = "";
+protected string scommandreader = "";
+protected StructSession stsession = new StructSession();
+protected StructConexion stconexion=new StructConexion();
+
+	}
+}
